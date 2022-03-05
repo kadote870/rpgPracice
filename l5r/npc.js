9fda3fcd7c870b461cdd8725404b35e3bf90273c
Karol Tomaszewski <kadote870@gmail.com>
2022-03-05 22:30:00 +0100

    l5k, npc generator nowe parametry które mająza zadanie zrobić postać bardziej kolorową
    - Skill w którym NPC jest mistrzem
    - 3 dodatkowe skille uzupełniające
    - Stan/profesja z któego wywodzi się NPC

--- l5r/npc.js
+++ l5r/npc.js
@@ -1,6 +1,4 @@
-function rollStat() {
-    return Math.floor((Math.random() * 5) + 1);
-}
+const rollStat = () => Math.floor((Math.random() * 5) + 1);
 
 const cechaWytrzymalosc = rollStat();
 const cechaSilaWoli = rollStat();
@@ -25,14 +23,45 @@ const umiejetnoscKenjutsu = rollStat();
 const katanaRoll = 3;
 const katanaKeep = 2;
 
+let ktoToJest;
+const ktoToJestRoll = Math.floor((Math.random() * 100) + 1);
+
+if (ktoToJestRoll < 5) {
+    ktoToJest = 'Kami'
+} else if (ktoToJestRoll < 10) {
+    ktoToJest = 'Ktoś z najbliższego otoczenia/rodziny cesarza';
+} else if (ktoToJestRoll < 15) {
+    ktoToJest = 'Ktoś z najbliższego otoczenia jednej z czołowych postaci poszczególnego klanu';
+} else if (ktoToJestRoll < 45) {
+    ktoToJest = 'Samuraj - Bushi/Shugenja';
+} else if (ktoToJestRoll < 65) {
+    ktoToJest = 'Kupiec / Rzemieślnik / Mnich';
+} else if (ktoToJestRoll < 85) {
+    ktoToJest = 'Chłop';
+} else {
+    ktoToJest = 'Eta';
+}
+
+const skillRandom = () => skills[Math.floor(Math.random() * skills.length)];
+
+const skills = ['Aktorstwo', 'Ceremonia parzenia herbaty', 'Dyplomacja', 'Etykieta', 'Fałszerstwo', 'Gawędziarstwo',
+    'Handel', 'Hazard', 'Heraldyka', 'Historia', 'Iaijutsu – pojedynek', 'Jeździectwo', 'Kaligrafia', 'Kyujutsu - Łuk',
+    'Malarstwo', 'Medycyna', 'Medytacja', 'Kenjutsu - Miecz', 'Miecznik', 'Muzyka', 'Obrona', 'Płatnerz', 'Poezja',
+    'Polowanie', 'Prawo', 'Rzemiosło', 'Shintao', 'Skradanie się', 'Szczerość', 'Śledztwo', 'Ślusarstwo', 'Taktyka',
+    'Taniec', 'Teologia', 'Torturowanie', 'Trucizny', 'Uwodzenie', 'Wachlarz bojowy', 'Walka wręcz', 'Wiedza',
+    'Wysportowanie', 'Zapasy', 'Zielarstwo'];
+
 const printer = (`
-Random NPC:
-Ziemia: ${kragZiemia}    | Wytrzymałość: ${cechaWytrzymalosc}   | Siła Woli: ${cechaSilaWoli}
-Ogień: ${kragOgnien}      | Zręczność: ${cechaZrecznosc}      | Inteligencja: ${cechaInteligencja}
-Woda: ${kragWoda}      | Siła: ${cechaSila}           | Spostrzegawczość: ${cechaSpostrzegawczosc}
-Powietrze: ${kragPowietrze}   | Refleks: ${cechaRefleks}       | Intuicja: ${cechaIntuicja}
+Random NPC: ${ktoToJest}
+Ziemia: ${kragZiemia} | Wytrzymałość: ${cechaWytrzymalosc} | Siła Woli: ${cechaSilaWoli}
+Ogień: ${kragOgnien} | Zręczność: ${cechaZrecznosc} | Inteligencja: ${cechaInteligencja}
+Woda: ${kragWoda} | Siła: ${cechaSila} | Spostrzegawczość: ${cechaSpostrzegawczosc}
+Powietrze: ${kragPowietrze} | Refleks: ${cechaRefleks} | Intuicja: ${cechaIntuicja}
 Pustka: ${kragPustka}
-            
+
+Mistrzostwo w: ${skillRandom()} 
+Inne istotne umiejętności: ${skillRandom()}, ${skillRandom()}, ${skillRandom()}   
+ 
 Miecz: ${umiejetnoscKenjutsu} (Atak: !${umiejetnoscKenjutsu + cechaZrecznosc}z${cechaZrecznosc}) | Obrażenia: (katana średniej jakości 3z2): !${katanaRoll + cechaSila}l${katanaKeep}
 Trudność Trafienia | Bez zbroi: ${cechaRefleks * 5} | Lekka zbroja: ${cechaRefleks * 5 + 5} | Ciężka zbroja: ${cechaRefleks * 5 + 10}
 Rany: ${kragZiemia * 2} na poziom | Rany całość: ${kragZiemia * 2 * 8}`);
