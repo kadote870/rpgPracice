8acdd6a2c3ee42eb1f6ac0b54eac9da48c3bd35f
kadote870 <kadote870@gmail.com>
2022-03-14 18:25:42 +0100

    l5k - tablice skills

--- l5r/modules/skills.js
+++ l5r/modules/skills.js
@@ -5,24 +5,16 @@ const skillsAll = ['Aktorstwo', 'Ceremonia parzenia herbaty', 'Dyplomacja', 'Ety
     'Taniec', 'Teologia', 'Torturowanie', 'Trucizny', 'Uwodzenie', 'Wachlarz bojowy', 'Walka wręcz', 'Wiedza',
     'Wysportowanie', 'Zapasy', 'Zielarstwo'];
 
+const skills = {
+    intuicja: ['Aktorstwo', 'Dyplomacja', 'Etykieta', 'Gawędziarstwo', 'Handel', 'Hazard', 'Iaijutsu – pojedynek', 'Jeździectwo', 'Muzyka', 'Poezja', 'Szczerość', 'Torturowanie', 'Uwodzenie'],
+    zrecznosc: ['Muzyka', 'Fałszerstwo', 'Jeździectwo', 'Miecz', 'Obrona', 'Skradanie się', 'Ślusarstwo', 'Taniec', 'Wachlarz bojowy', 'Walka wręcz'],
+    inteligencja: ['Historia', 'Malarstwo', 'Medycyna', 'Prawo', 'Shintao', 'Teologia', 'Trucizny', 'Wiedza', 'Zielarstwo'],
+    spostrzegawczosc: ['Heraldyka', 'Kaligrafia', 'Polowanie', 'Taktyka'],
+    pustka: ['Ceremonia parzenia herbaty', 'Medytacja'],
+    rozne: ['Rzemiosło', 'Śledztwo', 'Wysportowanie'],
+    refleks: ['Kyujutsu - Łuk'],
+    ziemia: ['Miecznik', 'Płatnerz'],
+    sila: ['Zapasy']
+}
+
 export const skillRandom = () => skillsAll[Math.floor(Math.random() * skillsAll.length)];
-
-const skillsInteligencja = ['Historia', 'Malarstwo', 'Medycyna', 'Prawo', 'Shintao', 'Teologia', 'Trucizny', 'Wiedza', 'Zielarstwo'];
-
-const skillsIntuicja = ['Aktorstwo', 'Dyplomacja', 'Etykieta', 'Gawędziarstwo', 'Handel', 'Hazard', 'Iaijutsu – pojedynek',
-    'Jeździectwo', 'Muzyka', 'Poezja', 'Szczerość', 'Torturowanie', 'Uwodzenie'];
-
-const skillsPustka = ['Ceremonia parzenia herbaty', 'Medytacja']
-
-const skillsRefleks = ['Kyujutsu - Łuk']
-
-const skillsRrozne = ['Rzemiosło', 'Śledztwo', 'Wysportowanie']
-
-const skillsSila = ['Zapasy']
-
-const skillsSpostrzegawczosc = ['Heraldyka', 'Kaligrafia', 'Polowanie', 'Taktyka']
-
-const skillsZiemia = ['Miecznik', 'Płatnerz']
-
-const skillsZrecznosc = ['Muzyka', 'Fałszerstwo', 'Jeździectwo', 'Miecz', 'Obrona', 'Skradanie się', 'Ślusarstwo', 'Taniec',
-    'Wachlarz bojowy', 'Walka wręcz']
