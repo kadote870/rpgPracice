10963753019d5354946538e852f6ca41feffa1e8
kadote870 <kadote870@gmail.com>
2022-07-03 14:54:31 +0200

    rope

--- roll/roll.js
+++ roll/roll.js
@@ -1,19 +1,19 @@
-export const roll_d5 = () => Math.floor((Math.random() * 5) + 1);
+export const roll_d5 = () => Math.floor(Math.random() * 5 + 1);
 
-export const roll_d6 = () => Math.floor((Math.random() * 6) + 1);
+export const roll_d6 = () => Math.floor(Math.random() * 6 + 1);
 
-const roll_d10 = () => Math.floor((Math.random() * 10) + 1);
+const roll_d10 = () => Math.floor(Math.random() * 10 + 1);
 
-export const roll_d100 = () => Math.floor((Math.random() * 100) + 1);
+export const roll_d100 = () => Math.floor(Math.random() * 100 + 1);
 
 export const roll_2d10_and_add_10 = () => {
-    return roll_d10() + roll_d10() + 10;
-}
+	return roll_d10() + roll_d10() + 10;
+};
 
 export const roll_2d10_and_add_20 = () => {
-    return roll_d10() + roll_d10() + 20;
-}
+	return roll_d10() + roll_d10() + 20;
+};
 
 export const roll_2d10_and_add_30 = () => {
-    return roll_d10() + roll_d10() + 30;
-}
+	return roll_d10() + roll_d10() + 30;
+};
